0ea32dbdcf3ad17c95a1b101079d6db2e2d8c9f2
shuaijianjian <zhoujianjian@channelsoft.com>
2017-12-20 16:44:44 +0800

    对AFNetworking断点下载的简单封装

--- Example/AFFileDownLoader/AFViewController.m
+++ Example/AFFileDownLoader/AFViewController.m
@@ -7,23 +7,76 @@
 //
 
 #import "AFViewController.h"
+#import "AFFileDownLoader.h"
 
-@interface AFViewController ()
+//30S：
+//http://saas.ccod.com:9090/record/LTBD2017050502/Agent/20171201/TEL-13811749890_BFYTT_6718_20171201150942.wav
+//
+//2分钟：
+//http://saas.ccod.com:9090/record/LTBD2017050502/Agent/20171201/TEL-13811749890_BFYTT_6718_20171201150942.wav
+//
+//10分钟：
+//http://saas.ccod.com:9090/record/LTBD2017050502/Agent/20171031/TEL-17600669657_BFYTT_6506_20171031150303.wav
+//
+//30分钟以上：
+//http://saas.ccod.com:9090/record/LTBD2017050502/Agent/20171010/TEL-13986180975_BFYTT_5411_20171010145757.wav
+
+@interface AFViewController ()<AFFileDownLoaderDelegate>
+
+@property (nonatomic, strong) AFFileDownLoader *downLoader;
+
+@property (weak, nonatomic) IBOutlet UITextView *iOSTextView;
+
 
 @end
 
 @implementation AFViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad{
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.iOSTextView.text = @"http://saas.ccod.com:9090/record/LTBD2017050502/Agent/20171010/TEL-13986180975_BFYTT_5411_20171010145757.wav";
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)downLoad:(UIButton *)sender {
+    self.downLoader = [[AFFileDownLoader alloc] init];
+    self.downLoader.delegate = self;
+    
+    NSURL *url = [NSURL URLWithString:self.iOSTextView.text];
+    
+    [self.downLoader downLoadWithFileUrl:url progress:^(CGFloat progress, long long fileTempSize, long long totalSize) {
+        NSLog(@"\nprogress => %f \nfileTempSize => %lld \ntotalSize => %lld",progress, fileTempSize, totalSize);
+    } success:^(NSString *downloadedFilePath) {
+        NSLog(@"downloadedFilePath => %@",downloadedFilePath);
+    } failure:^(NSError *error) {
+        NSLog(@"error => %@",error);
+    }];
 }
+
+- (void)downLoader:(AFFileDownLoader *)downLoader taskState:(AFSessionTaskState)taskState{
+    NSLog(@"下载状态 ==> taskState ==> 【%ld】", taskState);
+}
+
+/**
+ 取消下载
+ */
+- (IBAction)cancel:(UIButton *)sender {
+     [self.downLoader cancel];
+}
+
+/**
+ 暂停下载
+ */
+- (IBAction)suspend:(UIButton *)sender {
+    [self.downLoader suspend];
+}
+
+/**
+ 继续下载
+ */
+- (IBAction)resume:(UIButton *)sender {
+    [self.downLoader resume];
+}
+
+
 
 @end
---
+++ Example/Pods/Target Support Files/Pods-AFFileDownLoader_Example/Pods-AFFileDownLoader_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AFFileDownLoader_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AFFileDownLoader_Example
+@end
